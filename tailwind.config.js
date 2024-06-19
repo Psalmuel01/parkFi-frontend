ab7bf39aeae8ddab00be4609b2a3cb7d0219b16c
James Osunkiyesi <jaydhales@gmail.com>
2024-06-19 13:01:51 +0000

    Convert to ts

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,tsx,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#0D0C22',
+        'secondary': '#6366F1'
+      },
+    },
+  },
+  plugins: [],
+}
+
