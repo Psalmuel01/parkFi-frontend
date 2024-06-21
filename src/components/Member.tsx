ac1611859aaefcf3b21051014839edbbb0b7f95e
James Osunkiyesi <jaydhales@gmail.com>
2024-06-21 09:55:32 +0000

    fix

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,35 +1,43 @@
-import {SetStateAction, useState} from 'react'
-import Available from './Available';
-import MySpace from './MySpace';
+import { SetStateAction, useState } from "react";
+import Available from "./Available";
+import MySpace from "./MySpace";
 
 const Member = () => {
-    const [clickedButton, setClickedButton] = useState('available');
+  const [clickedButton, setClickedButton] = useState("available");
 
-    const handleClick = (button: SetStateAction<string>) => {
-        setClickedButton(button);
-    };
+  const handleClick = (button: SetStateAction<string>) => {
+    setClickedButton(button);
+  };
 
-    return (
-        <div>
-            <div className='mt-10 text-center text-2xl flex justify-center gap-60 font-medium'>
-                <button
-                    className={clickedButton === 'available' ? 'bg-primary px-4 py-2 rounded-lg text-white' : ''}
-                    onClick={() => handleClick('available')}
-                >
-                    Available Spaces
-                </button>
-                <button
-                    className={clickedButton === 'myspace' ? 'bg-primary px-4 py-2 rounded-lg text-white' : ''}
-                    onClick={() => handleClick('myspace')}
-                >
-                    My Space(s)
-                </button>
-            </div>
-            <div className='my-8'>
-                {clickedButton === 'available' ? <Available /> : <MySpace />}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="mt-10 text-center text-2xl flex justify-center gap-60 font-medium">
+        <button
+          className={
+            clickedButton === "available"
+              ? "bg-primary px-4 py-2 rounded-lg text-white"
+              : ""
+          }
+          onClick={() => handleClick("available")}
+        >
+          Available Spaces
+        </button>
+        <button
+          className={
+            clickedButton === "myspace"
+              ? "bg-primary px-4 py-2 rounded-lg text-white"
+              : ""
+          }
+          onClick={() => handleClick("myspace")}
+        >
+          My Booked Space(s)
+        </button>
+      </div>
+      <div className="my-8">
+        {clickedButton === "available" ? <Available /> : <MySpace />}
+      </div>
+    </div>
+  );
+};
 
-export default Member
+export default Member;
